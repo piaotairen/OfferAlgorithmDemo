7b0953cb369c4a193fb5a4f835ced6b98a36d7cd
ZiHai <xiataiyong@126.com>
2018-09-04 17:57:40 +0800

    update demo
    update demo

--- OfferAlgorithmDemo/Lesson 018/Lesson018.swift
+++ OfferAlgorithmDemo/Lesson 018/Lesson018.swift
@@ -7,3 +7,89 @@
 //
 
 import Foundation
+
+/// 链表节点
+class LinkNode {
+    var val: Int
+    
+    var prev: LinkNode?
+    
+    var next: LinkNode?
+    
+    init(_ val: Int) {
+        self.val = val
+        self.prev = nil
+        self.next = nil
+    }
+}
+
+/// 链表
+class LinkList {
+    var head: LinkNode?
+    
+    var tail: LinkNode?
+    
+    var count: Int = 0
+}
+
+/// 插入节点
+func insertNode(val: Int, index:Int, list: LinkList) {
+    let insertNode = LinkNode(val)
+    if list.count == 0 {
+        /// 空链表添加节点
+        list.head = insertNode
+        list.tail = insertNode
+    } else if list.count < index {
+        /// 插入位置不存在 插入尾节点
+        list.tail?.next = insertNode
+        insertNode.prev = list.tail
+        list.tail = insertNode
+    } else {
+        /// 插入指定位置
+        var searchNode = list.head
+        var num = 0
+        while num == list.count {
+            searchNode = searchNode?.next
+            num += 1
+        }
+        let temp = searchNode?.next
+        searchNode?.next = insertNode
+        insertNode.prev = searchNode
+        insertNode.next = temp
+        temp?.prev = insertNode
+    }
+    list.count += 1
+}
+
+/// 删除节点
+func removeNode(val: Int, list: LinkList) {
+    var removeNode = list.head
+    while removeNode != nil, removeNode!.val == val {
+        removeNode = removeNode?.next
+    }
+    if removeNode != nil {
+        if list.count == 1 {
+            /// 删除的节点为头结点和尾节点
+            
+        } else if list.count == 2 {
+            if removeNode === list.head {
+                /// 删除的节点为头结点
+                list.head = list.tail
+                list.tail?.prev = nil
+            } else {
+                /// 删除的节点为尾节点
+                list.tail = list.head
+                list.head?.next = nil
+            }
+        } else {
+            /// 删除的节点为中间节点
+            let prevNode = removeNode?.prev
+            let nextNode = removeNode?.next
+            prevNode?.next = nextNode
+            nextNode?.prev = prevNode
+        }
+        removeNode?.prev = nil
+        removeNode?.next = nil
+        list.count -= 1
+    }
+}
--- OfferAlgorithmDemo/Lesson 019/Lesson019.swift
+++ OfferAlgorithmDemo/Lesson 019/Lesson019.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+class TreeInvert {
+    
+    // 得到二叉树的镜像  —— 递归的方式
+    func mirror(root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        if root.left == nil && root.right == nil {
+            return
+        }
+        (root.left, root.right) = (root.right, root.left)
+        mirror(root: root.left)
+        mirror(root: root.right)
+    }
+    
+    // 得到二叉树的镜像 —— 不使用递归
+    func mirrorNotRecursive(root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        var stack: [TreeNode] = []
+        stack.append(root)
+        while stack.count != 0 {
+            let node = stack.removeFirst()
+            (node.left, node.right) = (node.right, node.left)
+            
+            if node.right != nil {
+                stack.append(node.right!)
+            }
+            if node.left != nil {
+                stack.append(node.left!)
+            }
+        }
+    }
+    
+    // 层次遍历二叉树
+    func levelTraverse(root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        var stack: [TreeNode] = []
+        stack.append(root)
+        while stack.count != 0 {
+            let node = stack.removeFirst()
+            print("var == \(node.val) *")
+            if node.left != nil {
+                stack.append(node.left!)
+            }
+            if node.right != nil {
+                stack.append(node.right!)
+            }
+        }
+    }
+    
+    /// 翻转二叉树
+    func invertTree(root: TreeNode?) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+        root.left = invertTree(root: root.left)
+        root.right = invertTree(root: root.right)
+        (root.left, root.right) = (root.right, root.left)
+        return root
+    }
+}
